45507ac2ab3f5dc5548b325ff22b0f67d6cc4559
Leonardo Lorieri <leonardo.lorieri@wal-mart.com>
2014-12-14 01:52:48 -0200

    fixes

--- deis-netstat.sh
+++ deis-netstat.sh
@@ -27,7 +27,8 @@ do
 	APP=`echo $d|cut -d/ -f4`
 
 	HOST=$COREOS_PRIVATE_IPV4
-	EDGE_HOST="{ data: { source: '$COREOS_PRIVATE_IPV4', target: '$APP' } },"
+	EDGE_HOST="{ data: { source: '$HOST', target: '$HOST-ntop', href: '' } },"
+        EDGE_HOST_NTOP="{ data: { source: '$HOST-ntop', target: '$APP', href: '' } },"
 
 	for i in `etcdctl --no-sync ls $d`;
 	do
@@ -77,7 +78,7 @@ do
 			LINKUNIT="http://$HOST:3000/lua/port_details.lua?port=$PORT"
 			LINKUNITIP="http://$HOST:3000/lua/host_details.lua?host=$IP"
 
-			
+
 
 			# { data: { id: 'j', name: 'Jerry' } },
 			# /lua/port_details.lua?port=44408
@@ -85,19 +86,26 @@ do
 
 			(
 				echo $EDGE_HOST;
+				echo $EDGE_HOST_NTOP;
 				echo $EDGE_APP;
-				echo "{ data: { source: '$UNIT', target: '$UNIT-$CONTYPE'} },";
+				echo "{ data: { source: '$UNIT', target: '$UNIT-ntop-ip'} },";
+				echo "{ data: { source: '$UNIT-ntop-ip', target: '$UNIT-ntop-port'} },";
+				echo "{ data: { source: '$UNIT-ntop-port', target: '$UNIT-$CONTYPE'} },";
                                 echo "{ data: { source: '$UNIT-$CONTYPE', target: '$UNIT-$CONTYPE-$CONQT'} },";
 				echo "{ data: { source: '$UNIT-$CONTYPE-$CONQT', target: 'all'} },";
 			) >> /tmp/netstat-paas-edges-json
 
 
 			(
-				echo "{ data: { id: '$HOST', name: '$HOST', fillcolor: 'blue', line: 'red', color: 'white', href: 'http://$HOST:3000/?page=TopPorts' } },";
-				echo "{ data: { id: '$APP', name: '$APP', fillcolor: '#00AAFF', line: '#33CC66', color: '#ff2366', href: '$LINKUNITIP' } },";
-        	                echo "{ data: { id: '$UNIT', name: '$UNIT' , fillcolor: '#33CC66' , line: '#ff2366', color: '#00AAFF', href: '$LINKUNIT' } },";
-				echo "{ data: { id: '$UNIT-$CONTYPE', name: '$CONTYPE' , fillcolor: '$TYPECOLOR' , line: '$TYPECOLOR', color: 'white', href: '$LINKUNIT' } },";
-                        	echo "{ data: { id: '$UNIT-$CONTYPE-$CONQT', name : '$CONQT' , fillcolor : '$CONCOLOR' , line : '#888', color : 'white', href: '$LINKUNIT' } },"
+				echo "{ data: { id: '$HOST', name: '$HOST', fillcolor: 'blue', line: 'red', color: 'white', href: '' } },";
+				echo "{ data: { id: '$HOST-ntop', name: 'ntop', fillcolor: 'blue', line: 'red', color: 'white', href: 'http://$HOST:3000/?page=TopPorts' } },";
+
+				echo "{ data: { id: '$APP', name: '$APP', fillcolor: '#00AAFF', line: '#33CC66', color: '#ff2366', href: '' } },";
+        	                echo "{ data: { id: '$UNIT', name: '$UNIT' , fillcolor: '#33CC66' , line: '#ff2366', color: '#00AAFF', href: '' } },";
+                                echo "{ data: { id: '$UNIT-ntop-port', name: 'ntop-port' , fillcolor: 'orange' , line: '$TYPECOLOR', color: 'white', href: '$LINKUNIT' } },";
+                                echo "{ data: { id: '$UNIT-ntop-ip', name: 'ntop-ip' , fillcolor: 'orange' , line: '$TYPECOLOR', color: 'white', href: '$LINKUNITIP' } },";
+				echo "{ data: { id: '$UNIT-$CONTYPE', name: '$CONTYPE' , fillcolor: '$TYPECOLOR' , line: '$TYPECOLOR', color: 'white', href: '' } },";
+                        	echo "{ data: { id: '$UNIT-$CONTYPE-$CONQT', name : '$CONQT' , fillcolor : '$CONCOLOR' , line : '#888', color : 'white', href: '' } },"
 			) >> /tmp/netstat-paas-nodes-json
 
 		done
